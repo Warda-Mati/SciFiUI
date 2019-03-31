8157e8e5dc8d3075e177fd8d63bc97382fcb87a3
Warda-Mati <c17323991@mydit.ie>
2019-03-31 20:04:25 +0100

    added rectangles below radar border

--- java/src/ie/tudublin/UI.java
+++ java/src/ie/tudublin/UI.java
@@ -1,31 +1,27 @@
 package ie.tudublin;
-
 import java.util.ArrayList;
 import processing.core.PApplet;
 public class UI extends PApplet
 {
-   // ArrayList<Radar> radars = new ArrayList<Radar>();
     ArrayList<RadarBorder> borders = new ArrayList<RadarBorder>(); // radar boerder with circle inside 
     ArrayList<SquareButtons> buttons = new ArrayList<SquareButtons>(); // square button at the bottom 
     ArrayList<Rectangles> brects = new ArrayList<Rectangles>(); // rects above line at the bottom  below square button 
     ArrayList<HorizontalLines> lines = new ArrayList<HorizontalLines>(); // small line onn the LHS 
     ArrayList<VerticlesLines> vline = new ArrayList<VerticlesLines>(); // verticle lines at the bottom below square buttons 
-   
-     
+    ArrayList<Rectangles> srects = new ArrayList<Rectangles>(); // small  narrow long rectangles  above squares  | horizontally drawn
+    ArrayList<Rectangles> vrects = new ArrayList<Rectangles>(); // small  narrow long rectangles  above squares  | vertically drawn
+    
     Button b;
     MovingCircle mc;
-    Radar r;
+   // Radar r;
     RadarBorder border;
-    RadarBorder border2;
-    RadarBorder border3;
-    SquareButtons button; // squares at the bottom 
+   // SquareButtons button; // squares at the bottom 
     HorizontalLines horizonline; // line at the bottom 
-    Rectangles rec; // small  narrow long rectangles  above squares  
+   // Rectangles rec; // small  narrow long rectangles  above squares  
     Rectangles rec1; // rectangles at the bottom of the square buttons
     Rectangles rec2; // first on the left corner with the text arrange 
 
     boolean[] keys = new boolean[1024];
-
     public void keyPressed()
     {
         keys[keyCode] = true;
@@ -51,39 +47,32 @@ public class UI extends PApplet
         colorMode(HSB);
         b = new Button(this, 50, 50, 100, 50, "I am a button");
         mc = new MovingCircle(this, width / 2, height / 2, 50);
-        r = new Radar(900, 700, 50, this);
+      //  r = new Radar(900, 700, 50, this);
         border = new RadarBorder(width/2 - 400,height/2 - 100 , 80, 10,this," Balanced");
-        border2 = new RadarBorder(width/2 +100,height/2 - 100,80,10,this,"Balanced");  
-        border3 = new RadarBorder(width/2 + 200, height/2-100, 80, 10, this, "Balanced"); 
-        rec = new Rectangles(700, 500, 400, 10,this, 125,89,0); // x , y, width, height , colour 
+       // rec = new Rectangles(700, 500, 400, 10,this, 125,89,0); // x , y, width, height , colour 
         rec1 = new Rectangles(50, 730, 100, 10, this,235,90,78);
         rec2 = new Rectangles(5, 650, 100, 20, this, 234,5,6);
-        button = new SquareButtons(400,500,50,this); 
+      //  button = new SquareButtons(400,500,50,this); 
         horizonline = new HorizontalLines(0, 750, width - 40 , this);
 
-        // for (int i=0; i < 10; i++) // circle | radar 
-        // {
-        //     Radar rr = new Radar(i *55, 50, 50, this);
-        //     radars.add(rr);
-        // }
+       
 
         for (int i=0; i < 9; i++) // radar border 
         {
-            RadarBorder bb = new RadarBorder(250+(i* 105) , height/2 +50, 80, 10, this , "Balance"); // x (distance), y, move up/down, size 
+            RadarBorder bb = new RadarBorder(250+(i* 105) , height/2 -50, 80, 10, this , "Balance"); // x (distance), y, move up/down, size 
             borders.add(bb);
         }
         for (int i=0; i < 16; i++) // Square buttons 
         {
             fill(255);
-            SquareButtons but = new SquareButtons(250+(i*60), 600, 50, this);
-            
+            SquareButtons but = new SquareButtons(250+(i*60), 600, 50, this);  
             buttons.add(but);
         }
 
 
         for (int i=0; i < 6; i++) //  rects at the bottom 
         {
-           Rectangles rec = new Rectangles(250+(i*150), 720, 135, 20, this,255,0,0); //width, move up/down,  distance , height 
+           Rectangles rec = new Rectangles(250+(i*160), 720, 150, 15, this,255,0,0); //width, move up/down,  distance , height 
            brects.add(rec);
         }
 
@@ -94,10 +83,34 @@ public class UI extends PApplet
         }
 
 
-        for (int i=0; i < 20; i++) // lines
+        for (int i=0; i < 20; i++) // lines 
         {
             VerticlesLines vline1 = new VerticlesLines(250+(i*50), 680, 20, this);
             vline.add(vline1); 
+        }
+
+        // for (int i=0; i < 5; i++) //  rects at the bottom  
+        // {   
+        //    Rectangles srec = new Rectangles(250+(i*100), 500, 400, 10, this, 125, 89, 0); //width, move up/down,  distance , height 
+        //    srects.add(srec); 
+        // }
+
+        for (int i=0; i < 3; i++) //  rects at the bottom  
+        {
+           Rectangles vrec = new Rectangles(250, 500+(i*20), 340, 6, this, 125, 89, 0); //width, move up/down,  distance , height 
+           vrects.add(vrec);
+        }
+
+        for (int i=0; i < 3; i++) //  rects at the bottom  
+        {
+           Rectangles vrec = new Rectangles(550, 500+(i*20), 340, 6, this, 125, 89, 0); //width, move up/down,  distance , height 
+           vrects.add(vrec);
+        }
+
+        for (int i=0; i < 3; i++) //  rects at the bottom  
+        {
+           Rectangles vrec = new Rectangles(860, 500+(i*20), 330, 6, this, 125, 89, 0); //width, move up/down,  distance , height 
+           vrects.add(vrec);
         }
 
 
@@ -111,21 +124,15 @@ public class UI extends PApplet
         // b.render();
         // mc.update();
         // mc.render();
-        r.render();
+      //  r.render();
         // border.render();
-        // border2.render();
-        // border3.render();
-        rec.render();
+       // rec.render();
         rec1.render();
         rec2.render();
-        button.render();
+     //   button.render();
         horizonline.render();
 
-        // for(Radar rr: radars) // radar , CIRCLE WITH LINES IN IT 
-        // {
-        //     rr.render();
-        // }
-
+      
         for(RadarBorder bb: borders) // radar border with circles inside 
         {
             bb.render();
@@ -156,6 +163,16 @@ public class UI extends PApplet
         for (VerticlesLines vline1: vline )
         {
             vline1.render();
+        }
+
+        // for(Rectangles srec: srects) // 
+        // {
+        //     srec.render();
+        // }
+
+        for(Rectangles vrec: vrects) // 
+        {
+            vrec.render();
         }
 
         
